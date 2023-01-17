3a13d1eb67bc178b645d26844780874145d5eca1
Milan Camus <pro@turtletv.fr>
2023-01-17 19:03:46 +0100

    ➖ Remove unused dependencies

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -15,5 +15,4 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
 ];
