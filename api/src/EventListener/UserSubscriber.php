a61ef4e106d245f0a3fc64f17755f209d2f20cbd
Milan Camus <pro@turtletv.fr>
2023-01-18 14:28:02 +0100

    🐛 Email template param

--- api/src/EventListener/UserSubscriber.php
+++ api/src/EventListener/UserSubscriber.php
@@ -36,7 +36,7 @@ class UserSubscriber implements EventSubscriberInterface {
       $this->updatePwd($object);
       $object->setToken($this->userRepository->generateToken());
       $object->setRoles(['ROLE_USER']);
-      $this->sendinblueMailer->sendEmail($object, ['firstname' => $object->getFirstname(), 'confirmationUrl' => $_ENV['FRONT_URL'] . '/confirm/' . $object->getToken()]);
+      $this->sendinblueMailer->sendEmail($object, ['firstname' => $object->getFirstname(), 'confirmationURL' => $_ENV['FRONT_URL'] . '/confirm/' . $object->getToken()]);
     }
   }
 
