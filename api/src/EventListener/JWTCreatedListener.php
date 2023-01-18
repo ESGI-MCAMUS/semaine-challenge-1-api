286dd4192773393b1e3bb8bacf00c3728459394f
Sulyk <sylvain.boudacher@gmail.com>
2023-01-18 16:45:53 +0100

    Change the token and data send when we login

---
+++ api/src/EventListener/JWTCreatedListener.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class JWTCreatedListener
+{
+    /**
+     * @var RequestStack
+     */
+    private $requestStack;
+    private $userRepository;
+
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+
+    /**
+     * @param JWTCreatedEvent $event
+     *
+     * @return void
+     */
+    public function onJWTCreated(JWTCreatedEvent $event): void
+    {
+
+        $user = $event->getUser();
+
+        $payload['id'] = $user->getId();
+        $payload['role'] = $user->getRoles();
+        $payload['email'] = $user->getEmail();
+        $payload['firstname'] = $user->getFirstname();
+        $payload['lastname'] = $user->getLastname();
+
+
+        $event->setData($payload);
+
+
+    }
+}
