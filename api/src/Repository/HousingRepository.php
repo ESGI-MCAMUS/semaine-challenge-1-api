b2dd44eefeb8da46b6f1eb574f7d7781b508cb7c
jeremy.jumpertz@gmail.com <jeremy.jumpertz@gmail.com>
2023-01-19 11:32:02 +0100

    [ADD] add housing filter

--- api/src/Repository/HousingRepository.php
+++ api/src/Repository/HousingRepository.php
@@ -39,28 +39,39 @@ class HousingRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return Housing[] Returns an array of Housing objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('h')
-//            ->andWhere('h.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('h.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    public function findByFilter($filter = null)
+    {
+        $qb = $this->createQueryBuilder('l');
+        if ($filter !== null) {
+            $qb->where("l.filter = :filter")
+                ->setParameter("filter", $filter);
+        }
+        return $qb->getQuery()->getResult();
+    }
 
-//    public function findOneBySomeField($value): ?Housing
-//    {
-//        return $this->createQueryBuilder('h')
-//            ->andWhere('h.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+
+    //    /**
+    //     * @return Housing[] Returns an array of Housing objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('h')
+    //            ->andWhere('h.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('h.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?Housing
+    //    {
+    //        return $this->createQueryBuilder('h')
+    //            ->andWhere('h.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
