2f9000bc387e29a79a898fe971a0cc177a5348b0
Romain Pierucci <pierucci.romain@gmail.com>
2023-02-10 23:37:56 +0100

    testing controller

--- api/src/Controller/CreateAppointment.php
+++ api/src/Controller/CreateAppointment.php
@@ -1,8 +1,42 @@
 <?php
 
-namespace App\Controller;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Routing\Annotation\Route;
 
-class CreateAppointment
+#[AsController]
+class CreateAppointment extends AbstractController
 {
 
+    private UserRepository $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    #[Route(
+        name: 'create_appointment',
+        path: '/appointment/{housingId}',
+        methods: ['POST'],
+        defaults: [
+            '_api_operation_name' => '_api_/appointment/{housingId}',
+            '_api_description' => 'Create Appointment',
+        ],
+    )]
+    public function __invoke(string $token): Response
+    {
+//        $user = $this->userRepository->findOneBy(['emailToken' => $token]);
+//        if (!$user) {
+//            return $this->json(['message' => 'Invalid token'], 400);
+//        }
+//        $user->setToken(null);
+//        $user->setIsActive(true);
+//        $this->userRepository->save($user, true);
+//        return $this->json($user);
+        return $this->json(['pouet' => 'test controller']);
+    }
 }
